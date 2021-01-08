MarcusGDaniels/lammps

commit 985fc86aa3f8e907c8dae04321db2b6464c007eb

use ev_init for kspace

src/KOKKOS/pppm_kokkos.cpp
@@ -615,8 +615,7 @@ void PPPMKokkos<DeviceType>::compute(int eflag, int vflag)
   // set energy/virial flags
   // invoke allocate_peratom() if needed for first time
 
-  if (eflag || vflag) ev_setup(eflag,vflag,0);
-  else evflag = evflag_atom = eflag_global = vflag_global =
+  ev_init(eflag,vflag,0);
          eflag_atom = vflag_atom = 0;
 
   // reallocate per-atom arrays if necessary

src/GPU/pppm_gpu.cpp
@@ -199,9 +199,7 @@ void PPPMGPU::compute(int eflag, int vflag)
   // set energy/virial flags
   // invoke allocate_peratom() if needed for first time
 
-  if (eflag || vflag) ev_setup(eflag,vflag);
-  else evflag = evflag_atom = eflag_global = vflag_global =
-        eflag_atom = vflag_atom = 0;
+  ev_init(eflag,vflag);
 
   // If need per-atom energies/virials, allocate per-atom arrays here
   // so that particle map on host can be done concurrently with GPU calculations

src/KSPACE/ewald.cpp
@@ -365,9 +365,7 @@ void Ewald::compute(int eflag, int vflag)
 
   // set energy/virial flags
 
-  if (eflag || vflag) ev_setup(eflag,vflag);
-  else evflag = evflag_atom = eflag_global = vflag_global =
-         eflag_atom = vflag_atom = 0;
+  ev_init(eflag,vflag);
 
   // if atom count has changed, update qsum and qsqsum
 

src/KSPACE/ewald_disp.cpp
@@ -688,8 +688,7 @@ void EwaldDisp::compute(int eflag, int vflag)
   // set energy/virial flags
   // invoke allocate_peratom() if needed for first time
 
-  if (eflag || vflag) ev_setup(eflag,vflag);
-  else evflag = eflag_global = vflag_global = eflag_atom = vflag_atom = 0;
+  ev_init(eflag,vflag);
 
   if (!peratom_allocate_flag && (eflag_atom || vflag_atom)) {
       allocate_peratom();

src/KSPACE/msm.cpp
@@ -454,9 +454,7 @@ void MSM::compute(int eflag, int vflag)
 
   // set energy/virial flags
 
-  if (eflag || vflag) ev_setup(eflag,vflag);
-  else evflag = evflag_atom = eflag_global = vflag_global =
-    eflag_atom = vflag_atom = eflag_either = vflag_either = 0;
+  ev_init(eflag,vflag);
 
   if (scalar_pressure_flag && vflag_either) {
     if (vflag_atom)

src/KSPACE/msm_cg.cpp
@@ -90,9 +90,7 @@ void MSMCG::compute(int eflag, int vflag)
 
   // set energy/virial flags
 
-  if (eflag || vflag) ev_setup(eflag,vflag);
-  else evflag = evflag_atom = eflag_global = vflag_global =
-    eflag_atom = vflag_atom = eflag_either = vflag_either = 0;
+  ev_init(eflag,vflag);
 
   // invoke allocate_peratom() if needed for first time
 

src/KSPACE/pppm.cpp
@@ -630,9 +630,7 @@ void PPPM::compute(int eflag, int vflag)
   // set energy/virial flags
   // invoke allocate_peratom() if needed for first time
 
-  if (eflag || vflag) ev_setup(eflag,vflag);
-  else evflag = evflag_atom = eflag_global = vflag_global =
-         eflag_atom = vflag_atom = 0;
+  ev_init(eflag,vflag);
 
   if (evflag_atom && !peratom_allocate_flag) {
     allocate_peratom();

src/KSPACE/pppm_cg.cpp
@@ -88,9 +88,7 @@ void PPPMCG::compute(int eflag, int vflag)
   // set energy/virial flags
   // invoke allocate_peratom() if needed for first time
 
-  if (eflag || vflag) ev_setup(eflag,vflag);
-  else evflag = evflag_atom = eflag_global = vflag_global =
-         eflag_atom = vflag_atom = 0;
+  ev_init(eflag,vflag);
 
   if (evflag_atom && !peratom_allocate_flag) {
     allocate_peratom();

commit 3b0270f77ed01567433fafef40d787b1d4a147bd

Fix bug in Kokkos PPPM per-atom introduced by 985fc86aa3f8e907c8dae04321db2b6464c007eb

src/KOKKOS/pppm_kokkos.cpp
@@ -579,7 +579,6 @@ void PPPMKokkos<DeviceType>::compute(int eflag, int vflag)
   // invoke allocate_peratom() if needed for first time
 
   ev_init(eflag,vflag,0);
-         eflag_atom = vflag_atom = 0;
 
   // reallocate per-atom arrays if necessary
 